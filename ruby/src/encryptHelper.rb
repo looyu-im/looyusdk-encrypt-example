3e6fc80610ce4e5bb4437d1ee792facf03f4398e
caijian <cai_jian@looyu.com>
2017-09-30 03:47:10 +0800

    ruby version

--- ruby/src/encryptHelper.rb
+++ ruby/src/encryptHelper.rb
@@ -3,25 +3,26 @@ require 'base64'
 require 'openssl'
 require 'digest'
 
+$iv="0123456789ABCDEF"
 class EncryptHelper
   def initialize()
 
   end
 
-  def encrypt(blank, key)
+  def self.encrypt(blank, key)
     cipher = OpenSSL::Cipher.new("AES-128-CBC")
     cipher.encrypt
-    cipher.iv = "0123456789ABCDEF"
+    cipher.iv = $iv
     cipher.key = Digest::MD5.digest(key)
     encrypted = cipher.update(blank) + cipher.final
     Base64.encode64(encrypted).chomp
   end
 
-  def decrypt(encrypted, key)
+  def self.decrypt(encrypted, key)
     encryptedText = Base64.decode64(encrypted)
     cipher = OpenSSL::Cipher.new("AES-128-CBC")
     cipher.decrypt
-    cipher.iv = "0123456789ABCDEF"
+    cipher.iv = $iv
     cipher.key = Digest::MD5.digest(key)
     blank = cipher.update(encryptedText) + cipher.final
     blank.force_encoding("iso-8859-1").force_encoding('utf-8')
